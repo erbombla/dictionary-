214c47236a3166d5faece3542f78bff55c015cc8
Eric <ebomblatus@gmail.com>
2016-12-02 13:39:15 -0800

    pass specs for all, save and clear methods

--- lib/word.rb
+++ lib/word.rb
@@ -2,7 +2,22 @@ class Word
   @@words = []
   attr_reader(:word)
 
+#Class Initialization
   def initialize(word)
     @word = word.capitalize
   end
-end
+
+#Instance Methods
+  def save
+    @@words.push(self)
+  end
+
+#Class Methods
+  def self.all
+    @@words
+  end
+
+  def self.clear
+    @@words =[]
+  end
+end 
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -5,8 +5,30 @@ require 'word'
 describe 'Word' do
   describe '#word' do
     it 'returns a word' do
-      test_word = Word.new('Gumption')
-      expect(test_word.word).to eq('Gumption')
+      test_word = Word.new('Loquacious')
+      expect(test_word.word).to eq('Loquacious')
     end
   end
+
+  describe '.all' do
+    it 'initializes empty array' do
+      expect(Word.all).to eq([])
+    end
+  end
+
+  describe '#save' do
+    it 'saves a word to the array' do
+      test_word = Word.new('Loquacious')
+      expect(test_word.save).to eq([test_word])
+    end
+  end
+
+  describe '.clear' do
+    it 'clears the array' do
+      test_word = Word.new('Loquacious')
+      test_word.save
+      expect(Word.clear).to eq([])
+    end
+  end
+
 end
