18c526d39bad1b078e5c1779e688ab71db28ce00
Eric Bomblatus <ebomblatus@gmail.com>
2016-12-06 16:43:06 -0800

    create app.rb and corresponding erb files. Add content to dictionary, definition form and word success files

--- app.rb
+++ app.rb
@@ -1,8 +1,47 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require './lib/template'
+require './lib/word'
+require './lib/definition'
 also_reload 'lib/**/*.rb'
 
+get '/dictionary' do
+  @words = Word.all
+  erb :dictionary
+end
+
 get '/' do
-  erb :index
+  @words.all
+  erb :dictionary
+end
+
+get '/words/new' do
+  erb :word_form
+end
+
+post '/words' do
+  new_word = Word.new({word_title: params[:new_word]})
+  new_word.save
+  erb :word_success
+end
+
+get '/dictionary/:id' do
+  @word = Word.find_word(params[:id])
+  erb :definition_form
+end
+
+get '/dictionary/:id/new_definition' do
+  @word = Word.find_word(params[:id])
+  erb :definition_form
+end
+
+post '/definition' do
+  @word = Word.find_word(params[:id])
+  new_definition = Definition.new({pOs: params[:pOs], definition: params[:definition]})
+  @word.add_definition(new_definition)
+  new_definition.save
+  erb :definiton_success
+end
+
+get 'definition' do
+  erb :word
 end
