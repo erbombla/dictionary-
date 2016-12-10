ec299695f260881d9d3f27f4c3b155ea81d2cd53
Eric <ebomblatus@gmail.com>
2016-12-09 16:56:45 -0800

    add integration spec but fails rspec

--- spec/dict_integration_spec.rb
+++ spec/dict_integration_spec.rb
@@ -1,31 +1,31 @@
-# require 'capybara/rspec'
-# require './app'
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-#
-# describe('/', {:type => :feature}) do
-#   it('prompts user to add more words') do
-#     visit('/')
-#     click_button('Add Word')
-#     expect(page).to have_content('Personal Dictionary Your Dictionary Saved Words Add Word')
-#   end
-# end
-#
-# describe('/word_form', {:type => :feature}) do
-#   it('recieves a word from the user') do
-#     visit('/word_form')
-#     fill_in('word', :with => 'stridency')
-#     click_button('Add')
-#     expect(page).to have_content('output')
-#   end
-# end
-#
-# describe('/definition_form', {:type => :feature}) do
-#   it('recieves a definition from the user') do
-#     visit('/definition/stridency')
-#     fill_in('definition', :with => 'presenting a point of view in a forceful way')
-#     fill_in('pOs', :with => 'noun')
-#     click_button('Add')
-#     expect(page).to have_content('output')
-#   end
-# end
+require 'capybara/rspec'
+require './app'
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('/', {:type => :feature}) do
+  it('prompts user to create new entry') do
+    visit('/')
+    click_button('Add')
+    expect(page).to have_content('New Entry')
+  end
+end
+
+describe('/word_form', {:type => :feature}) do
+  it('recieves a word from the user') do
+    visit('/word_form')
+    fill_in('user_word', :with => 'stridency')
+    click_button('Add')
+    expect(page).to have_content('output')
+  end
+end
+
+describe('/definition_form', {:type => :feature}) do
+  it('recieves a definition from the user') do
+    visit('/definition/stridency')
+    fill_in('user_definition', :with => 'presenting a point of view in a forceful way')
+    fill_in('user_pOs', :with => 'noun')
+    click_button('Add')
+    expect(page).to have_content('output')
+  end
+end
