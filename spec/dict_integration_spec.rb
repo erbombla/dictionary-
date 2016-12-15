84d0e5ca72f62bc0927733d9e48a86099ce51d45
Eric Bomblatus <ebomblatus@gmail.com>
2016-12-14 16:12:38 -0800

    pass integration specs

--- spec/dict_integration_spec.rb
+++ spec/dict_integration_spec.rb
@@ -1,4 +1,28 @@
-# require 'capybara/rspec'
-# require './app'
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
+require 'capybara/rspec'
+require './app'
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the word path', {:type => :feature}) do
+  it('recieves a word') do
+    visit('/')
+    fill_in("user_word", :with => 'stridency')
+    click_button('Add')
+  end
+end
+
+describe('the definition path', {:type => :feature}) do
+  it('recieves a definition') do
+    visit('/')
+    fill_in("user_word", :with => 'stridency')
+    click_button('Add')
+  end
+end
+
+describe('the dictionary path', {:type => :feature}) do
+  it('displays the word and definition') do
+    visit('/')
+    fill_in("user_word", :with => 'stridency')
+    click_button('Add')
+  end
+end
